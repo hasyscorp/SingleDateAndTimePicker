d677afbf73003003243414e3cbbd2c9e85ace4ec
Wayde.K <wayde.k@kakaocorp.com>
2019-06-24 18:14:49 +0900

    + background change + back key bug fix

--- singledateandtimepicker/src/main/java/com/github/florent37/singledateandtimepicker/dialog/DoubleDateAndTimePickerDialog.java
+++ singledateandtimepicker/src/main/java/com/github/florent37/singledateandtimepicker/dialog/DoubleDateAndTimePickerDialog.java
@@ -8,6 +8,7 @@ import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -98,7 +99,16 @@ public class DoubleDateAndTimePickerDialog extends BaseDialog {
     }
 
     private void init(View view) {
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    close();
+                }
 
+                return true;
+            }
+        });
 
         buttonTab0 = (TextView) view.findViewById(R.id.buttonTab0);
         buttonTab1 = (TextView) view.findViewById(R.id.buttonTab1);
